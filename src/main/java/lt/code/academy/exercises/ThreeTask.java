1927d5dfcca9844d3c1cdeb4ac040a62c3c90fe8
gamblerLt <rimas.spakauskas@outlook.com>
2023-02-02 16:37:27 +0200

    11T

--- src/main/java/lt/code/academy/exercises/ThreeTask.java
+++ src/main/java/lt/code/academy/exercises/ThreeTask.java
@@ -1,6 +1,46 @@
 package lt.code.academy.exercises;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ThreeTask {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Iveskite skacius (0 daro abrakadabra): ");
+        int number = scanner.nextInt();
+
+        int[] array = new int[number];
+        for (int i = 0; i < number; i++) {
+
+        }
+
+
+
+
+        /*
+        int[] array = new int[userInput];
+        System.out.println("Iveskite " + userInput + " skaicius");
+        for (int i = 0; i < userInput; i++) {
+            array[i] = scanner.nextInt();
+
+        }
+
+        System.out.println("I masyva ivedete sius skaicius:");
+
+        for (int i = 0; i < userInput; i++) {
+            System.out.print(array[i] + ", ");
+        }
+
+        System.out.println("\n********");*/
+
+    }
+
+    public class sum {
+
+
+    }
+
 
 }
 /*
